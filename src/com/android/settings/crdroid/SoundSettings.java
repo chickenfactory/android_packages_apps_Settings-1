176f8b43c339243a45807e2b95bec3452e525039
chickenfactory <micah.chickenfactory@gmail.com>
2015-03-01 13:05:43 -1000

    fix copyright

--- src/com/android/settings/crdroid/SoundSettings.java
+++ src/com/android/settings/crdroid/SoundSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 crDroid Project
+ * Copyright (C) 2013 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
